fb7b5984ee1d883ff2b0ba99e13c1fc0e52a0e36
Vladimir Iglovikov <iglovikov@gmail.com>
2018-09-02 23:33:04 -0700

    New format for InplaceABN compilation.

--- modules/misc.py
+++ modules/misc.py
@@ -1,4 +1,4 @@
-import torch.nn as nn
+from torch import nn
 
 
 class GlobalAvgPool2d(nn.Module):
--- modules/residual.py
+++ modules/residual.py
@@ -1,6 +1,7 @@
+from __future__ import absolute_import
 from collections import OrderedDict
 
-import torch.nn as nn
+from torch import nn
 
 from .bn import ABN
 
